b883bd43208531adc08d03dcd57b19f1e14ba86f
Locietta <locietta@gmail.com>
2025-06-24 12:26:42 +0800

    misc: register header files in xmake for IDE display

--- src/process/xmake.lua
+++ src/process/xmake.lua
@@ -3,6 +3,7 @@
 target("process")
     set_kind("static")
     add_files("*.cpp")
+    add_headerfiles("*.h", { install = false })
     add_packages("fmt")
     if is_os("windows") then
         add_syslinks("kernel32", "user32", "shell32")
