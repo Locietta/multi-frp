4f48e479367d3fa5470428d1281738a0b0b4a549
Locietta <locietta@gmail.com>
2026-01-14 21:14:39 +0800

    ci: inject version string on ci

--- src/xmake.lua
+++ src/xmake.lua
@@ -4,8 +4,15 @@ includes("process")
 
 add_requires("fmt", "nlohmann_json", "argparse")
 
+option("version")
+    set_showmenu(true)
+    set_default("dev")
+    set_description("version string to embed in the build")
+    add_defines("MULTI_FRP_VERSION=\"$(version)\"")
+
 target("multi-frp")
     add_rules("module.program")
+    add_options("version")
     add_files("*.cpp")
     add_packages("fmt", "nlohmann_json", "argparse")
     add_headerfiles("*.h", "util/*.hpp", { install = false })
