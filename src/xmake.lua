b8e1c4b470f5a025434afa3a6ce1e43d962fc92a
Locietta <locietta@gmail.com>
2026-01-14 09:38:19 +0800

    chore: disable upx
    it bloats binary on linux, and has no significant benefit on windows

--- src/xmake.lua
+++ src/xmake.lua
@@ -3,7 +3,7 @@ add_includedirs(".")
 includes("process")
 
 target("multi-frp")
-    add_rules("module.program", {upx = true})
+    add_rules("module.program")
     add_files("*.cpp")
     add_packages("fmt", "nlohmann_json")
     add_headerfiles("*.h", "util/*.hpp", { install = false })
