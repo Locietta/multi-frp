1ff634bb7aff2f78aa456890927e6a6470ef050a
Locietta <locietta@gmail.com>
2026-01-14 09:50:48 +0800

    xmake: improve module.program rule when upx is not enabled

--- xmake/upx.lua
+++ xmake/upx.lua
@@ -5,9 +5,6 @@ rule("module.program")
 
     after_build(function (target)
         local enabled = target:extraconf("rules", "module.program", "upx")
-        if (not enabled) or (not is_mode("release")) then
-            return
-        end
 
         import("core.project.depend")
         import("lib.detect.find_tool")
@@ -15,9 +12,13 @@ rule("module.program")
         local targetfile = target:targetfile()
         depend.on_changed(function ()
             local file = path.join("build", path.filename(targetfile))
-            local upx = assert(find_tool("upx"), "upx not found!")
-
             os.tryrm(file)
-            os.vrunv(upx.program, {targetfile, "-o", file})
+
+            if (not enabled) or (not is_mode("release")) then
+                os.cp(targetfile, file)
+            else
+                local upx = assert(find_tool("upx"), "upx not found!")
+                os.vrunv(upx.program, {targetfile, "-o", file})
+            end
         end, {files = targetfile})
     end)
