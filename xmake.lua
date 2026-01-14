70327cb2b85d12c32d0ac399c73bfb56d49624fb
Locietta <locietta@gmail.com>
2026-01-14 10:01:44 +0800

    xmake: enable lto

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,7 @@ set_description("Simple wrapper for frp to connect to multiple servers")
 
 add_rules("mode.release", "mode.debug", "mode.releasedbg")
 set_languages("cxx23")
+set_policy("build.optimization.lto", true)
 
 if is_os("windows") then
     set_toolchains("clang-cl")
