64bb417b4900442f80f0fab48ce4e20cab3cfdd9
Locietta <locietta@gmail.com>
2026-01-18 10:07:21 +0800

    misc: add file io support for print
    also improve the interface design a bit

--- src/util/print.hpp
+++ src/util/print.hpp
@@ -5,20 +5,23 @@
 #include <cstring>
 
 // print a bunch of strings at once
-// first gather all the length of the strings then allocate a buffer
+// first gather all the length of the strings to allocate a buffer
 // then call write to print them all at once
-template <typename... Args>
-constexpr void print(std::string_view format, Args&&... args) {
-    const auto total_size = (format.size() + ... + std::string_view(args).size());
-    // debug
-    // std::printf("Total size: %zu\n", total_size);
+template <std::convertible_to<std::string_view>... Args>
+constexpr void fprint(FILE *file, Args &&...args) {
+    const auto total_size = (std::string_view(args).size() + ...);
     char *buffer = new char[total_size + 1];
     char *ptr = buffer;
-    for (const auto &str : {format, std::string_view(args)...}) {
+    for (const auto &str : {std::string_view(args)...}) {
         std::memcpy(ptr, str.data(), str.size());
         ptr += str.size();
     }
     buffer[total_size] = '\0';
-    std::fwrite(buffer, 1, total_size, stdout);
+    std::fwrite(buffer, 1, total_size, file);
     delete[] buffer;
+}
+
+template <std::convertible_to<std::string_view>... Args>
+constexpr void print(Args &&...args) {
+    fprint(stdout, std::forward<Args>(args)...);
 }
