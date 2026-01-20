f32de0abbc7132ee4493e13c6297243768b39aba
Locietta <locietta@gmail.com>
2026-01-21 00:22:11 +0800

    xmake: changed manifest should trigger rebuild

--- xmake/manifest.lua
+++ xmake/manifest.lua
@@ -7,4 +7,12 @@ rule("embed.manifest")
     end)
     on_buildcmd_file(function (target, batchcmds, sourcefile, opt)
         target:add("ldflags", "/manifestinput:" .. sourcefile, {force = true})
+
+        local target_file = target:targetfile()
+
+        -- add depend for manifest file, so that target will be rebuilt when manifest changed
+        batchcmds:add_depfiles(sourcefile)
+        batchcmds:set_depmtime(os.mtime(target_file))
+        batchcmds:set_depcache(target:dependfile(target_file))
     end)
+rule_end()
